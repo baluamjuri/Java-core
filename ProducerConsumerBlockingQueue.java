81400352153cb24af198de814e0bb998d7699704
baluamjuri <43845769+baluamjuri@users.noreply.github.com>
2022-05-27 21:55:11 +0530

    Producer consumer

---
+++ ProducerConsumerBlockingQueue.java
@@ -0,0 +1,38 @@
+package com.copart.b2b;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ProducerConsumerBlockingQueue {
+	public static void main(String[] args) throws InterruptedException {
+		BlockingQueue<Integer> queue = new LinkedBlockingQueue<>(1); 
+		Thread producer = new Thread(() -> {
+			for(int i=1;i<=10;i++) {
+				try {
+					queue.put(i);
+					System.out.println(Thread.currentThread()+" put "+i);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		Thread consumer = new Thread(() ->{
+			for(int i=0;i<10;i++) {
+				try {
+					Thread.sleep(1000);
+					int n = queue.take();
+					System.out.println(Thread.currentThread()+" got "+ n);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		producer.start();
+		consumer.start();	
+		producer.join();
+		consumer.join();
+	}
+}
+
---
+++ ProducerConsumerClassicStyle.java
@@ -0,0 +1,82 @@
+package com.copart.b2b;
+
+public class ProducerConsumerClassicStyle {
+	public static void main(String[] args) throws InterruptedException {
+		Item item = new Item();
+		Thread producer = new Thread(new Producer(item));
+		Thread consumer = new Thread(new Consumer(item));
+		producer.start();
+		consumer.start();	
+		producer.join();
+		consumer.join();
+	}
+}
+
+class Producer implements Runnable{
+	private Item item;
+	public Producer(Item item) {
+		this.item = item;
+	}
+	
+	@Override
+	public void run() {
+		for(int i=1;i<=10;i++) {
+			try {
+				item.put(i);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		synchronized (item) {//Last iteration of item.get() waits
+			item.notify();
+		}
+	}
+}
+
+class Consumer implements Runnable{
+	private Item item;
+	public Consumer(Item item) {
+		this.item = item;
+	}
+	
+	@Override
+	public void run() {
+		for(int i=0;i<10;i++) {
+			try {
+				item.get();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+class Item{
+	private int n;
+	private volatile boolean available;
+	
+	public synchronized void put(int n) throws InterruptedException {
+		if(available) {
+			wait();
+		}else {
+			this.n = n;
+			available = true;
+			System.out.println(Thread.currentThread()+" put "+n);
+			notify();
+			wait();
+		}
+	}
+	
+	public synchronized void get() throws InterruptedException {
+		if(available) {
+			System.out.println(Thread.currentThread()+" got "+ n);
+			available = false;
+			notify();
+			wait();
+		}else {
+			wait();
+		}
+	}
+}
